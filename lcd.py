efc50b6b905c4f61bf75c8eb88d615ac7d784e46
Jason-bolt <appiatujason@gmail.com>
2022-08-11 13:36:48 +0100

    LCD displaying readings of the two DHT11 sensors

---
+++ lcd.py
@@ -0,0 +1,47 @@
+# requires RPi_I2C_driver.py
+import RPi_I2C_driver
+from time import *
+
+# DHT11 libraries
+import sys
+import Adafruit_DHT
+
+
+# DTH pins
+top_pin = 17
+bottom_pin = 27
+
+# Getting the temperature and humidity for both DHTs
+top_humidity, top_temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, top_pin)
+bottom_humidity, bottom_temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, bottom_pin)
+
+
+# Initializing the lcd
+mylcd = RPi_I2C_driver.lcd()
+
+# Top sensor readings
+mylcd.lcd_display_string("T-Hum:" + str(top_humidity) + "%", 1)
+mylcd.lcd_display_string("T-Temp:" + str(top_temperature) + "C", 2)
+
+sleep(5) # Wait for 5 seconds
+
+mylcd.lcd_clear() # Clear screen
+sleep(1) # Wait for1 second
+
+
+# Bottom sensor readings
+mylcd.lcd_display_string("B-Hum:" + str(bottom_humidity) + "%", 1)
+mylcd.lcd_display_string("B-Temp:" + str(bottom_temperature) + "C", 2)
+
+sleep(5) # Wait for 5 seconds
+
+mylcd.lcd_clear() # Clear screen
+sleep(1) # Wait for1 second
+
+
+
+
+
+
+
+mylcd.backlight(0)
