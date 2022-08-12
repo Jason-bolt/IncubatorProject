e69262146748ebabdfb19e57ff3f45da2efc2751
Jason-bolt <appiatujason@gmail.com>
2022-08-12 15:28:26 +0100

    Added outside DHT11

--- lcd.py
+++ lcd.py
@@ -10,10 +10,12 @@ import Adafruit_DHT
 # DTH pins
 top_pin = 17
 bottom_pin = 27
+outside_pin = 22
 
 # Getting the temperature and humidity for both DHTs
 top_humidity, top_temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, top_pin)
 bottom_humidity, bottom_temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, bottom_pin)
+outside_humidity, outside_temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, outside_pin)
 
 
 # Initializing the lcd
@@ -39,6 +41,14 @@ mylcd.lcd_clear() # Clear screen
 sleep(1) # Wait for1 second
 
 
+# Outside sensor readings
+mylcd.lcd_display_string("O-Hum:" + str(outside_humidity) + "%", 1)
+mylcd.lcd_display_string("O-Temp:" + str(outside_temperature) + "C", 2)
+
+sleep(7) # Wait for 5 seconds
+
+mylcd.lcd_clear() # Clear screen
+sleep(1) # Wait for1 second
 
 
 
