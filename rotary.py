850984552909c46657c8cd92991d946ff62e7727
Jason-bolt <appiatujason@gmail.com>
2022-08-22 13:50:07 +0100

    Added rotary function

---
+++ rotary.py
@@ -0,0 +1,34 @@
+from RPi import GPIO
+from time import sleep
+
+clk = 23
+dt = 24
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+counter = 0
+
+clkState = GPIO.input(clk)
+dtState = GPIO.input(dt)
+
+previousValue = True
+
+while True:
+	if previousValue != GPIO.input(clk):
+		if GPIO.input(clk) == False:
+			if GPIO.input(dt) == False:
+				counter += 1
+				print (counter)
+			else:
+				counter -= 1
+				if counter < 0:
+					counter = 0
+				print (counter)
+
+
+	previousValue = GPIO.input(clk)
+
