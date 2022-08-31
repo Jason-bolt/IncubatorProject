44840f383cc7b1e8b6026ef4c48bc28ce3aaa0c2
Jason-bolt <appiatujason@gmail.com>
2022-08-31 22:47:10 +0100

    Relay successful

--- relay.py
+++ relay.py
@@ -2,7 +2,7 @@ from RPi import GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-pin = 23
+pin = 18
 
 ON = GPIO.LOW
 OFF = GPIO.HIGH
@@ -10,9 +10,9 @@ OFF = GPIO.HIGH
 GPIO.setup(pin, GPIO.OUT)
 
 GPIO.output(pin, ON)
-time.sleep(5)
+time.sleep(50)
 GPIO.output(pin, OFF)
-time.sleep(5)
+time.sleep(1)
 
 GPIO.cleanup()
 
